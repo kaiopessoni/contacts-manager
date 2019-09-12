76bd4cf4a76552906e8636fcad952b50342f4846
Kaio Pessoni <kaiopessoni@gmail.com>
2019-09-12 16:21:08 -0300

    Add columns classes to show contact view be responsive

--- resources/views/contacts/show.blade.php
+++ resources/views/contacts/show.blade.php
@@ -7,51 +7,51 @@
     <div class="card-body p-5">
 
       <div class="row align-items-center mb-4">
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-phone"></i> <span class="attribute">Phone:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->phone}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->phone}}</div>
 
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-envelope-o"></i> <span class="attribute">Email:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->email}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->email}}</div>
       </div>
 
       <div class="row align-items-center mb-4">
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-file-text-o"></i> <span class="attribute">Zip Code:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->zipcode}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->zipcode}}</div>
 
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-globe"></i> <span class="attribute">State:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->state}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->state}}</div>
       </div>
 
       <div class="row align-items-center mb-4">
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-map-o"></i> <span class="attribute">City:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->city}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->city}}</div>
 
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-building-o"></i> <span class="attribute">Neighbourhood:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->neighbourhood}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->neighbourhood}}</div>
       </div>
 
       <div class="row align-items-center mb-4">
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-flag-o"></i> <span class="attribute">Street:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->street}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->street}}</div>
 
-        <div class="col-lg-2">
+        <div class="col-lg-2 col-5">
           <i class="fa fa-home"></i> <span class="attribute">Number:</span>
         </div>
-        <div class="col-lg-4 info-value text-muted">{{$contact->number}}</div>
+        <div class="col-lg-4 col-7 info-value text-muted">{{$contact->number}}</div>
       </div>
 
     </div>
